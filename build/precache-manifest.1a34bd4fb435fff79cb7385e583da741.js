3b497d335eaa7e97f96b1f08e5e8db1eeba0d51e
Otso Helos <17408120+otsohelos@users.noreply.github.com>
2020-04-07 14:08:59 +0300

    front is here

---
+++ build/precache-manifest.1a34bd4fb435fff79cb7385e583da741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969dc52ac8e69a82eed8073d7649142c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eb9847fa2aa6ff1f809",
+    "url": "/static/css/main.9840da36.chunk.css"
+  },
+  {
+    "revision": "7aa309bc599dd39eb63e",
+    "url": "/static/js/2.038d7cde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.038d7cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb9847fa2aa6ff1f809",
+    "url": "/static/js/main.a4f53d8a.chunk.js"
+  },
+  {
+    "revision": "e448cfec73e89011fc66",
+    "url": "/static/js/runtime-main.e25a60c8.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
+
 app.use(cors())
 //const apodRouter = require('./controllers/apod')
 const http = require('http')
