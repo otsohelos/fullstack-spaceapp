c585a3dd669fd371086c14c7ccb360cc403cf3f4
Otso Helos <17408120+otsohelos@users.noreply.github.com>
2021-01-20 15:24:40 +0200

    update npm, remove some lines in marsWeather that cause errors

--- build/precache-manifest.eb4bfa0a96eeeb8ca4bc3fb2dda8a84e.js
+++ build/precache-manifest.eb4bfa0a96eeeb8ca4bc3fb2dda8a84e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f2883034f1802a7e696111567332ff7f",
+    "revision": "6948a61d1bcae6dcdc0735f0a11f3c89",
     "url": "/index.html"
   },
   {
-    "revision": "99d20d2b0fc703f197fb",
+    "revision": "1f8c7109cf80e29ddc48",
     "url": "/static/css/main.fb9b6b11.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.03f861b0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "99d20d2b0fc703f197fb",
-    "url": "/static/js/main.44318ba3.chunk.js"
+    "revision": "1f8c7109cf80e29ddc48",
+    "url": "/static/js/main.bb3fc8cf.chunk.js"
   },
   {
     "revision": "e448cfec73e89011fc66",
